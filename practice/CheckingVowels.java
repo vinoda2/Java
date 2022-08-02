6925b16695d5550a17f858f81bf5cc620a48dff7
vinoda2 <109263358+vinoda2@users.noreply.github.com>
2022-08-02 10:26:23 +0530

    Add files via upload

---
+++ practice/CheckingVowels.java
@@ -0,0 +1,11 @@
+package com.xworkz.practice;
+
+public class CheckingVowels {
+
+	static String s1="Xworkz";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ practice/StringLength.java
@@ -0,0 +1,15 @@
+package com.xworkz.practice;
+
+//finding length of a string Using length()
+public class StringLength {
+	static String s1="Vinoda";
+	
+	//length function to find the length of the String
+	static int l=s1.length();
+	
+	public static void main(String args[]) {
+	    //printing length
+		System.out.println(l);
+	}
+
+}
